bc4880a8a0686f2ec103c54a51d49f0993e5dd1c
ShreyasK412 <61613199+ShreyasK412@users.noreply.github.com>
2020-02-28 22:37:52 -0800

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,12 @@
+
+public class Runner {
+     public static void main(String[] args){
+         
+         Generator Gen= new Generator(10);
+         
+         
+         System.out.println(Gen.Creator( Gen.Instantiator()));
+        
+        
+     }
+}
