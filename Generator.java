eea1026eb7dadfec286b8f3192986b4c3777320e
Sanjit Sarda <sanjitsarda4@gmail.com>
2020-03-30 18:22:51 -0700

    psswdGenerator

--- Generator.java
+++ Generator.java
@@ -12,69 +12,34 @@ public class Generator {
     }
 
     public ArrayList<String> Instantiator() {
- String[] Letters = new String[]{ "A","B", "C", "D","E", "F", "G", "H","I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T","U" ,"V","W", "X","Y", "Z" };        
- String[] Symbols = new String[]{"!","@","#","$","%","&","_"};
+        String[] Letters = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+                "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        String[] Symbols = new String[] { "!", "@", "#", "$", "%", "&", "_" };
 
- Random random = new Random();
-        
-        
         ArrayList<String> DataBase = new ArrayList<String>();
-        for (int i = 0; i < 100; i++) {
-            
-            
-            String str = String.valueOf(i);
-            DataBase.add(str);
-            if(i<Symbols.length){
-            for(int z=0;z<5;z++){    
-            DataBase.add(i,Symbols[i]);
-            }
-            }
-            else if(i<Letters.length){
-                DataBase.add(i,Letters[i]);
-                
-            }
-            
+        for (int i = 0; i < Symbols.length + Letters.length + 10; i++) {
+            if (i < 10) {
+                String str = String.valueOf(i);
+                DataBase.add(str);
+            } else if (i < Symbols.length) {
+                for (int z = 0; z < 5; z++) {
+                    DataBase.add(i, Symbols[i]);
+                }
+            } else if (i < Letters.length) {
+                DataBase.add(i, Letters[i]);
 
+            }
         }
-        DataBase.add("a");
-        DataBase.add("b");
-        DataBase.add("c");
-        DataBase.add("d");
-        DataBase.add("e");
-        DataBase.add("f");
-        DataBase.add("g");
-        DataBase.add("h");
-        DataBase.add("i");
-        DataBase.add("j");
-        DataBase.add("k");
-        DataBase.add("l");
-        DataBase.add("m");
-        DataBase.add("n");
-        DataBase.add("o");
-        DataBase.add("p");
-        DataBase.add("q");
-        DataBase.add("r");
-        DataBase.add("s");
-        DataBase.add("t");
-        DataBase.add("u");
-        DataBase.add("v");
-        DataBase.add("w");
-        DataBase.add("x");
-        DataBase.add("y");
-        DataBase.add("z");
-        
-        
-    
+
         return DataBase;
-        
+
     }
 
     public String Creator(ArrayList<String> DataBase) {
-        
-      
+
         Random random = new Random();
         int initial = random.nextInt(DataBase.size());
-        String Pass = "";
         String PassW = "";
 
         for (int j = 0; j < length; j++) {
--- Runner.java
+++ Runner.java
@@ -1,17 +1,16 @@
 import static java.lang.Integer.parseInt;
 import java.util.Scanner;
-public class Runner {
-     public static void main(String[] args){
-         Scanner myObj = new Scanner(System.in); 
-    System.out.println("Enter How Many Terms Your Password Needs");
 
-    String Length = myObj.nextLine();  
-     int Len= parseInt(Length);
-         Generator Gen= new Generator(Len);
-         
-         
-         System.out.println(Gen.Creator( Gen.Instantiator()));
-        
-        
-     }
+public class Runner {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter How Many Terms Your Password Needs");
+
+        String Length = myObj.nextLine();
+        int Len = parseInt(Length);
+        Generator Gen = new Generator(Len);
+
+        System.out.println(Gen.Creator(Gen.Instantiator()));
+
+    }
 }
