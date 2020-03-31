9cdb6850d276cb208f2bd0f0c995d75e1f391b41
Sanjit Sarda <sanjitsarda4@gmail.com>
2020-03-30 18:33:36 -0700

    Forgot to commit

--- Generator.java
+++ Generator.java
@@ -1,9 +1,7 @@
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Generator {
-
     private int length;
 
     Generator(int aLength) {
@@ -16,37 +14,26 @@ public class Generator {
                 "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         String[] Symbols = new String[] { "!", "@", "#", "$", "%", "&", "_" };
-
         ArrayList<String> DataBase = new ArrayList<String>();
         for (int i = 0; i < Symbols.length + Letters.length + 10; i++) {
             if (i < 10) {
-                String str = String.valueOf(i);
-                DataBase.add(str);
-            } else if (i < Symbols.length) {
-                for (int z = 0; z < 5; z++) {
-                    DataBase.add(i, Symbols[i]);
-                }
-            } else if (i < Letters.length) {
-                DataBase.add(i, Letters[i]);
-
+                DataBase.add(String.valueOf(i));
+            } else if (i < Symbols.length + 10) {
+                DataBase.add(Symbols[i - 10]);
+            } else if (i < Letters.length + Symbols.length + 10) {
+                DataBase.add(Letters[i - 10 - Symbols.length]);
             }
         }
-
         return DataBase;
-
     }
 
     public String Creator(ArrayList<String> DataBase) {
-
         Random random = new Random();
         int initial = random.nextInt(DataBase.size());
-        String PassW = "";
-
+        String PassW = "j";
         for (int j = 0; j < length; j++) {
             int index = random.nextInt(DataBase.size());
-
             String Part = DataBase.get(index);
-
             PassW += Part;
         }
         return PassW;
--- Runner.java
+++ Runner.java
@@ -5,12 +5,9 @@ public class Runner {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter How Many Terms Your Password Needs");
-
         String Length = myObj.nextLine();
         int Len = parseInt(Length);
         Generator Gen = new Generator(Len);
-
         System.out.println(Gen.Creator(Gen.Instantiator()));
-
     }
 }
