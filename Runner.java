48c681ebe4c94eba978a322140cf54ada47317dd
Sanjit Sarda <sanjitsarda4@gmail.com>
2020-03-30 18:42:40 -0700

    yes

--- Runner.java
+++ Runner.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner makichod = new Scanner(System.in);
         System.out.println("Enter How Many Terms Your Password Needs");
-        String Length = myObj.nextLine();
+        String Length = makichod.nextLine();
         int Len = parseInt(Length);
         Generator Gen = new Generator(Len);
         System.out.println(Gen.Creator(Gen.Instantiator()));
